b9c96352109a1633e5cc8e1e277c089a4fc9c12c
hebedebe <babyyodaismine@gmail.com>
2025-05-13 12:50:37 +1000

    spritestacking fixes

--- umbra/Umbra/Misc/SpriteStack.cpp
+++ umbra/Umbra/Misc/SpriteStack.cpp
@@ -17,17 +17,17 @@ SpriteStack::~SpriteStack()
 
 void SpriteStack::Render(Vector2 pos, float rotation, float scale, Color color) const
 {
-	for (int i = 0; i < m_textures.size(); i++)
+	for (size_t i = 0; i < m_textures.size(); i++)
 	{
 		Texture tex = m_textures[i];
 
 		// Define source and destination rectangles
-		Rectangle source = { 0, 0, (float)tex.width, (float)tex.height };
-		Vector2 origin = { tex.width / 2.0f * scale, tex.height / 2.0f * scale };
-		Vector2 destPos = Vector2Add(pos, { 0, m_ySeparation * i });
+		const Rectangle source = { 0, 0, static_cast<float>(tex.width), static_cast<float>(tex.height) };
+		const Vector2 origin = { tex.width / 2.0f * scale, tex.height / 2.0f * scale };
+		auto [x, y] = Vector2Add(pos, { 0, m_ySeparation * i });
 
 		// Apply scaling in DrawTexturePro directly
-		Rectangle dest = { destPos.x, destPos.y, tex.width * scale, tex.height * scale };
+		const Rectangle dest = { x, y, tex.width * scale, tex.height * scale };
 
 		// Draw with center-based rotation
 		DrawTexturePro(tex, source, dest, origin, rotation, color);
